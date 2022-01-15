de880498df93c2aebc377505e91be73ce14cb0c1
Yixin Zheng <yixinzh@umich.edu>
2022-01-14 23:14:27 -0500

    Load data from interface to database

--- src/main/java/com/zheng/seckill/controller/SeckillActivityController.java
+++ src/main/java/com/zheng/seckill/controller/SeckillActivityController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -20,7 +21,39 @@ import java.util.Map;
 public class SeckillActivityController {
 
     @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
+    @Autowired
     private SeckillCommodityDao seckillCommodityDao;
+
+    @ResponseBody
+    @RequestMapping("/addSeckillActivityAction")
+    public String addSeckillActivityAction(
+            @RequestParam("name") String name,
+            @RequestParam("commodityId") long commodityId,
+            @RequestParam("seckillPrice") BigDecimal seckillPrice,
+            @RequestParam("oldPrice") BigDecimal oldPrice,
+            @RequestParam("seckillNumber") long seckillNumber,
+            @RequestParam("startTime") String startTime,
+            @RequestParam("endTime") String endTime
+    ) throws ParseException {
+        startTime = startTime.substring(0, 10) + startTime.substring(11);
+        endTime = endTime.substring(0, 10) + endTime.substring(11);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-ddhh:mm");
+        SeckillActivity seckillActivity = new SeckillActivity();
+        seckillActivity.setName(name);
+        seckillActivity.setCommodityId(commodityId);
+        seckillActivity.setSeckillPrice(seckillPrice);
+        seckillActivity.setOldPrice(oldPrice);
+        seckillActivity.setTotalStock(seckillNumber);
+        seckillActivity.setAvailableStock(new Integer("" + seckillNumber));
+        seckillActivity.setLockStock(0L);
+        seckillActivity.setActivityStatus(1);
+        seckillActivity.setStartTime(format.parse(startTime));
+        seckillActivity.setEndTime(format.parse(endTime));
+        seckillActivityDao.inertSeckillActivity(seckillActivity);
+        return seckillActivity.toString();
+    }
 
 
     @RequestMapping("/addSeckillActivity")
